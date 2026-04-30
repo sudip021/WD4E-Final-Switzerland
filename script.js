27212eb5e6d17885d246d25bd8cd38a67c989e80
sudip021 <sudiphaldar8742@gmail.com>
2026-04-30 08:06:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,33 @@ function upDate(previewPic) {
 function undo() {
     let imageDiv = document.getElementById("image");
 
-    imageDiv.innerHTML = "Hover over an image below to display here.";
+    imageDiv.innerHTML = "Hover or focus on an image below to display here.";
     imageDiv.style.backgroundImage = "url('')";
 }
+
 function showFact() {
     document.getElementById("fact").innerHTML =
         "Switzerland has over 1,500 lakes and is one of the most peaceful countries in the world.";
 }
+
+/* REQUIRED FUNCTION */
+function addTabFocus() {
+    console.log("Page loaded: tabindex applied");
+
+    let images = document.querySelectorAll(".gallery img");
+
+    for (let i = 0; i < images.length; i++) {
+        images[i].setAttribute("tabindex", "0");
+
+        images[i].onfocus = function () {
+            upDate(this);
+        };
+
+        images[i].onblur = function () {
+            undo();
+        };
+    }
+}
+
+/* ONLOAD EVENT */
+window.onload = addTabFocus;
